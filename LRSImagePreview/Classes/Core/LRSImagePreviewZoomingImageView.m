10e1ae72e0a4fdb06f0d866ac6c8d10401fa23b0
刘sama <junchen.liu@jiamiantech.com>
2021-08-20 10:36:21 +0800

    增加动画

--- LRSImagePreview/Classes/Core/LRSImagePreviewZoomingImageView.m
+++ LRSImagePreview/Classes/Core/LRSImagePreviewZoomingImageView.m
@@ -34,11 +34,16 @@
     [view.containerView addSubview:view.imageView];
     view.imageView.image = configure.placeholderImage;
     if (configure.hightURL) {
+        UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithFrame:view.bounds];
+        indicator.hidesWhenStopped = true;
+        [view addSubview:indicator];
+        [indicator startAnimating];
         [[SDWebImageManager sharedManager] loadImageWithURL:configure.hightURL options:(SDWebImageHighPriority | SDWebImageDelayPlaceholder) progress:nil completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
             if (finished && image) {
                 view.imageView.image = image;
                 [view resizeSubviewsWithImageSize:image.size];
             }
+            [indicator stopAnimating];
         }];
     }
     return view;
